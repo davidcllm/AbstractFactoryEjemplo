e07bf44090c9a8068a316b45ec9aa037c03933d9
davidcllm <davcm24@hotmail.com>
2026-02-07 20:04:26 -0600

    Creación clase cliente

---
+++ src/main/java/org/example/abstractfactoryejemplo/Cliente.java
@@ -0,0 +1,25 @@
+package org.example.abstractfactoryejemplo;
+
+import javafx.scene.Parent;
+import org.example.abstractfactoryejemplo.factory.RestWareFactory;
+import org.example.abstractfactoryejemplo.interfaces.InterfazGrafica;
+import org.example.abstractfactoryejemplo.interfaces.ReporteFinanciero;
+
+public class Cliente {
+    private InterfazGrafica interfaz;
+    private ReporteFinanciero reporte;
+
+    // El cliente recibe la fábrica por inyectar dependencias
+    public Cliente(RestWareFactory factory) {
+        this.interfaz = factory.crearInterfaz();
+        this.reporte = factory.crearReporte();
+    }
+
+    public Parent obtenerVista() {
+        return interfaz.cargarVista();
+    }
+
+    public void ejecutarReporte() {
+        reporte.generar();
+    }
+}
